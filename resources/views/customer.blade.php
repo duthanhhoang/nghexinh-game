61f6b2590ab7c2f272733d90f17f318e040fba10
duthanhhoang <duthanhhoang@gmail.com>
2023-02-27 15:48:08 +0700

    update

--- resources/views/customer.blade.php
+++ resources/views/customer.blade.php
@@ -207,7 +207,7 @@
         crossorigin="anonymous"></script>
 <script src="js/scripts.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
-<script src="js/datatables-simple-demo.js"></script>
+<script src="{{asset('js/datatables-simple-demo.js')}}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
 <script>
     $(document).ready(function () {
